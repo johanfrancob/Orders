edf74770ed4b6712852ccb31547db9cc56f9b402
JOHAN CAMILO FRANCO BEDOYA <johanfranco323357@correo.itm.edu.co>
2025-08-15 17:09:25 -0500

    Country class created and DataContext added.

---
+++ Orders/Orders.Backend/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Orders.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Orders.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+
+        }
+    }
+}
